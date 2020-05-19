863893c6ce595f1ec4349b54e5460f18fc29173c
yige142 <279290309@qq.com>
2020-05-19 14:21:15 +0800

    调试图片放大镜效果

--- upview.php
+++ upview.php
@@ -3,12 +3,12 @@ header("Content-type:text/html;charset=utf-8");
 class UpView{
 
 
-//    private $dbms='mysql';     //数据库类型
-//    private $host='127.0.0.1'; //数据库主机名
-//    private $dbName='wq_laserfair_com';    //使用的数据库
-//    private $user='root';      //数据库连接用户名
-//    private $pass='root';          //对应的密码
-//    private $mysqli='';
+    private $dbms='mysql';     //数据库类型
+    private $host='127.0.0.1'; //数据库主机名
+    private $dbName='wq_laserfair_com';    //使用的数据库
+    private $user='root';      //数据库连接用户名
+    private $pass='root';          //对应的密码
+    private $mysqli='';
 
 
     public function mysqlConn(){
@@ -25,13 +25,22 @@ class UpView{
 
 
 }
-$a=mt_rand(2,5);
+//更新观看随机数
+$a=mt_rand(1,5);
 $up =  new UpView();
+
 $mysqli=$up->mysqlConn();
 try {
 
-    $sql="UPDATE `ims_fy_lesson_parent` set `visit_number`= `visit_number`+$a";
+    //根据视频的记录条数的范围内 随机更新
+    $sql_="SELECT COUNT(*) FROM `ims_fy_lesson_parent`";
+    $count = $mysqli->query($sql_)->fetch_assoc();
+
+    $limit=mt_rand(0,$count['COUNT(*)']);
+
+    $sql="UPDATE `ims_fy_lesson_parent` set `visit_number`= `visit_number`+{$a} limit {$limit}";
     $result = $mysqli->query($sql);
+
 
 } catch (Exception $e) {
    // echo $e->getMessage();
