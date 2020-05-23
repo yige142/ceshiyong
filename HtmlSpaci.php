e4017917e55ee1ae840a6a859fd1b4a0d55c1301
yige142 <279290309@qq.com>
2020-05-23 22:33:22 +0800

    测试html标签存储用htmlspacicha函数过滤，调试云直播，用云直播js+div，实现直播画面正常播放

--- HtmlSpaci.php
+++ HtmlSpaci.php
@@ -12,27 +12,27 @@ $base=new BaseSql();
 //连接数据库
 $mysqli = $base->mysqlConn('laser_shop');
 
-$sql = "SELECT * FROM `destoon_article_data_21` limit 10";
+$sql = "SELECT * FROM `destoon_article_data_21` limit 1";
 $result = $mysqli->query($sql);
 
 if($result){
     $row = $result->fetch_all(MYSQLI_ASSOC);
     $count_row=count($row);
 
-    foreach($row as $key =>$value){
-        $content=str_replace('<a href=\'http://www.v550.com\'>&nbsp; </a>', '', $value['content']);
-
-        $sql="UPDATE `destoon_article_data_21` set `content`= {$content} WHERE `itemid`={$value['itemid']}";
-        $result = $mysqli->query($sql);
-        echo $result;
-    }
+//    foreach($row as $key =>$value){
+//        $content=str_replace('<a href=\'http://www.v550.com\'>&nbsp; </a>', '', $value['content']);
+//
+//        $sql="UPDATE `destoon_article_data_21` set `content`= {$content} WHERE `itemid`={$value['itemid']}";
+//        $result = $mysqli->query($sql);
+//        echo $result;
+//    }
 
 //    var_dump($row);die();
 }
 
 
-//$a=htmlspecialchars($row[0]['content'],ENT_COMPAT);
-//$b=str_replace('<a href=\'http://www.v550.com\'>&nbsp; </a>', '', $row[0]['content']);
-//
-//echo $b;die();
+$a=htmlspecialchars($row[0]['content']);
+$b=str_replace('<a href=\'http://www.v550.com\'>&nbsp; </a>', '', $row[0]['content']);
+$c=htmlentities($a);
+echo htmlspecialchars_decode($b) ;die();
 //var_dump($a);
