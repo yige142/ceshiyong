1707c0ed9982b3b87f64c4368453348fd6f6402e
yige142 <279290309@qq.com>
2020-05-26 17:59:38 +0800

    调试截取字符串

---
+++ PHP/str.php
@@ -0,0 +1,11 @@
+<?php
+
+//截取字符串
+//$a="http://www.laser123.com/file/upload/202005/26/102827501.png.thumb.png";
+//$b=substr($a,0,-10);
+//echo $b;
+
+
+$a="http://old.laserfair.com/uploads/userup/8651/14194a334-b11.jpg";
+$b=strpos($a,'old');
+echo $b;
