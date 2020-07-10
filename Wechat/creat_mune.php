4cc7fe0118dc67d7607990b51278b764a89c88ee
yige142 <279290309@qq.com>
2020-07-10 14:29:46 +0800

    调试微信端增加菜单

---
+++ Wechat/creat_mune.php
@@ -0,0 +1,12 @@
+<?php
+$access_token="35_i-0C0X0uWH7FfqLtOlB9w55B9tnj489mAaAIpqNkbrT7BYpnkhgQ100oVXUVLOxyltU-sHON-k-sidTGonVNTpTiZFAJDdFb7ahj-s_ptJe0R1Skyu6T2-Kwj4_vwUJ2UlOFxjovZ_zB9xIKMFUcABAATI";
+
+$body="{
+    \"button\": [
+        {
+            \"type\": \"view\", 
+            \"name\": \"首页\", 
+            \"url\": \"http://192.168.1.32/ceshiyong/wechat/index.php\"
+        }
+    ]
+}";
---
+++ Wechat/index.php
@@ -0,0 +1,3 @@
+<?php
+echo 123;
+var_dump($_SESSION);
