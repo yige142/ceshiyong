bdd522497aca89df2c8764e094fbbf7d5e0f9e8b
yige142 <279290309@qq.com>
2020-10-08 17:40:40 +0800

    调测jquery加载更多功能，调测用php获取db数组的排名功能

---
+++ PHP/array_search.php
@@ -0,0 +1,31 @@
+<?php
+
+//获取uid 排名
+$userdb = array(
+    0 => array(
+        'uid' => 100,
+        'name' => 'Sandra Shush',
+        'url' => 'urlof100'
+    ),
+
+    1 => array(
+        'uid' => 5465,
+        'name' => 'Stefanie Mcmohn',
+        'pic_square' => 'urlof100'
+    ),
+
+    2 => Array(
+        'uid' => 40489,
+        'name' => 'Michael',
+        'pic_square' => 'urlof40489'
+    )
+);
+
+$found_key = array_search(5465, array_column($userdb, 'uid'));
+/**
+如果$userdb很大，建议使用一个变量，避免搜索每个元素时都调用array_column()
+$uid = array_column($userdb, 'uid');
+$found_key = array_search(40489, $uid);
+ */
+var_dump($found_key);
+var_dump(array_column($userdb, 'uid'));
