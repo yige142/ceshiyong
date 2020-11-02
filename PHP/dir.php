d769bf0b59513c765b4bd6f38509ae4321c97537
yige142 <279290309@qq.com>
2020-11-02 15:41:26 +0800

    调试文件引用正斜杠反斜杠

--- PHP/dir.php
+++ PHP/dir.php
@@ -1,1 +1,7 @@
 <?php
+header('Content-type: text/html; charset=UTF8');
+//调试文件引用 正斜杠 反斜杠问题
+define('IA_ROOT', str_replace('\\', '/', dirname(dirname(__FILE__))));
+var_dump('file路径：'.dirname(__FILE__));
+var_dump('file路径：'.dirname(dirname(__FILE__)));
+var_dump(IA_ROOT);
